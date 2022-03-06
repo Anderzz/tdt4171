2f55f43f2b527f877e6f014ec100c2fab4c3cab5
Anderzz <akjelsrud@gmail.com>
2022-03-06 18:52:59 +0100

    progress?

--- assignment4/decisiontree.py
+++ assignment4/decisiontree.py
@@ -11,16 +11,23 @@ target = '1.5'
 
 class Node():
     '''
-    Node class, for root and internal nodes. Leaves are values pure
+    Node class, for root and internal nodes. Leaves are values
     '''
     def __init__(self, attr, values):
-        #super().__init__()
         self.attribute = attr
         self.values = values
         self.children = []
     
     def get_attribute(self):
         return self.attribute
+
+    def print_children(self):
+        for child in self.children:
+            print(child)
+
+def tree_construct(attr, att_value_list):
+    tree_node = Node(attr, att_value_list.get(attr))
+    return tree_node
 
 def B(q):
     return 0 if q == 1 or q == 0 else -(q*np.log2(q) + (1-q)*np.log2(1-q))
@@ -46,31 +53,26 @@ def remainder(T, A, p, n, examples):
     return sum
 
 def importance(tar, A, examples, rand=False):
+    print(examples[tar].value_counts())
     if rand: return random.uniform(0,1) 
     #positive
-    p = examples[tar].value_counts()[1]
+    try:
+        p = examples[tar].value_counts()[1]
+    except:
+        p = 0
+    #p=random.randint(40,60)
     #negative
-    n = examples[tar].value_counts()[2]
+    try:
+        n = examples[tar].value_counts()[2]
+    except:
+        n = 0
+    #n=random.randint(20,39)
     return B(p/(p+n)) - remainder(tar, A, p, n, examples)
 
 def get_id():
     return (''.join(random.choices(string.ascii_lowercase, k=4)))
 
-"""class Node:
-    def __init__(self, attribute, split=None):
-        self.attribute = attribute
-        self.children = []
-        self.split = split"""
 
-def get_attribute(self):
-    return self.attributes
-
-def tree_construct(attr, att_value_list):
-    '''
-    Trekonstruktør, lager node med attributt og verdier satt.
-    '''
-    tree_node = Node(attr, att_value_list.get(attr))
-    return tree_node
 
 def learn_decision_tree(examples, attributes, graph=None, parent_examples=()):
 
@@ -93,18 +95,20 @@ def learn_decision_tree(examples, attributes, graph=None, parent_examples=()):
     #find the most important attribute
     gain = {}
     for col in attributes:
-        gain[col] = importance(target, col, examples, rand=True)
-    print(gain)
+        gain[col] = importance(target, col, examples, rand=False)
+    #print(gain)
     
     #A = max(gain, key = gain.get)
     max_list = sorted(gain.items(), key=lambda x: x[1], reverse=True)
     A = max_list[1][0] if max_list[0][0] == target else max_list[0][0]
     tree = tree_construct(A,attribute_value_pairs)
     for v in tree.values:#v in examples[A].unique():
-        exs = examples[examples[A]==v]
-        print(exs)
-        #new_attrs = attributes.copy().drop(A)
-        #subtree = learn_decision_tree(exs, new_attrs, graph, examples)
+        #exs = examples[examples[A]==v]
+        exs = examples[examples[tree.attribute]==v]
+        new_attrs = deepcopy(attributes).drop(tree.get_attribute())
+        subtree = learn_decision_tree(exs, new_attrs, graph, examples)
+        tree.children.append((subtree, v))
+    return tree
 
 
 
@@ -133,7 +137,8 @@ def main():
     #tree.render(view=True)
 
 
-    learn_decision_tree(train,train.columns,tree)
+    res = learn_decision_tree(train,train.columns,tree)
+    res.print_children()
 
 
 
