933c407a9aba679ec6f63881e172cb7c998ab692
Anders Kjelsrud <akjelsrud@gmail.com>
2022-03-07 14:52:07 +0100

    progress!

--- assignment4/decisiontree.py
+++ assignment4/decisiontree.py
@@ -1,33 +1,23 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from graphviz import Digraph
 import random
 import string
-from copy import deepcopy
+import uuid
 
 random.seed(0)
 target = '1.5'
 
-class Node():
-    '''
-    Node class, for root and internal nodes. Leaves are values
-    '''
-    def __init__(self, attr, values):
-        self.attribute = attr
-        self.values = values
+class Node:
+    """
+    Class Node
+    """
+    def __init__(self, value):
         self.children = []
-    
-    def get_attribute(self):
-        return self.attribute
+        self.value = value
+        self.id = str(value)
+            
 
-    def print_children(self):
-        for child in self.children:
-            print(child)
-
-def tree_construct(attr, att_value_list):
-    tree_node = Node(attr, att_value_list.get(attr))
-    return tree_node
 
 def B(q):
     return 0 if q == 1 or q == 0 else -(q*np.log2(q) + (1-q)*np.log2(1-q))
@@ -48,12 +38,11 @@ def remainder(T, A, p, n, examples):
             nk = split[T].value_counts()[2]
         except:
             nk = 0
-
+    
         sum += ((pk + nk)/(p + n)) * B(pk/(pk + nk))
     return sum
 
 def importance(tar, A, examples, rand=False):
-    print(examples[tar].value_counts())
     if rand: return random.uniform(0,1) 
     #positive
     try:
@@ -67,6 +56,7 @@ def importance(tar, A, examples, rand=False):
     except:
         n = 0
     #n=random.randint(20,39)
+    #print(f"pos: {p}, neg: {n}")
     return B(p/(p+n)) - remainder(tar, A, p, n, examples)
 
 def get_id():
@@ -74,13 +64,11 @@ def get_id():
 
 
 
-def learn_decision_tree(examples, attributes, graph=None, parent_examples=()):
-
-    attribute_value_pairs = {}
-    for att in attributes:
-        attribute_value_pairs[att] = examples[att].unique()
-
-    if len(examples) == 0:
+def learn_decision_tree(examples, attributes, tree=None, parent_examples=()):
+    if len(examples) == 1 or len(examples) == 0:
+        value = examples[target].unique()[0]
+        id = str(uuid.uuid1())
+        tree.node(id, label=str(value))
         return plurality_values(parent_examples)
 
 
@@ -93,28 +81,34 @@ def learn_decision_tree(examples, attributes, graph=None, parent_examples=()):
         return plurality_values(examples)
     
     #find the most important attribute
-    gain = {}
-    for col in attributes:
-        gain[col] = importance(target, col, examples, rand=False)
-    #print(gain)
+    else:
+        dict = {}
+        gain = {}
+        for col in attributes:
+            gain[col] = importance(target, col, examples, rand=True)
+        #print(gain)
+        
+        max_list = sorted(gain.items(), key=lambda x: x[1], reverse=True)
+        max_list = list(filter(lambda x: x[0] != '1.5', max_list))
+        A = max_list[0][0]
+        #print(gain, f"velg {A}")
+        id = str(uuid.uuid1())
+        tree.node(id, label=A)
+        dict_list = {}
+        for v in examples[A].unique():
+            exs = examples[examples[A]==v]
+            new_attrs = attributes.copy().drop(A)
+            subtree = learn_decision_tree(exs, new_attrs, tree, examples)
+            tree.edge(id, subtree[0], label=str(v))
+            dict_list[v] = subtree[1]
     
-    #A = max(gain, key = gain.get)
-    max_list = sorted(gain.items(), key=lambda x: x[1], reverse=True)
-    A = max_list[1][0] if max_list[0][0] == target else max_list[0][0]
-    tree = tree_construct(A,attribute_value_pairs)
-    for v in tree.values:#v in examples[A].unique():
-        #exs = examples[examples[A]==v]
-        exs = examples[examples[tree.attribute]==v]
-        new_attrs = deepcopy(attributes).drop(tree.get_attribute())
-        subtree = learn_decision_tree(exs, new_attrs, graph, examples)
-        tree.children.append((subtree, v))
-    return tree
+        return [id, dict]
 
 
 
 def plurality_values(e):
-    return  e[target].mode()[0]
-    #kanskje prøv e[target].value_counts().idxmax()
+    id = str(uuid.uuid1())
+    return  [id, e[target].mode()[0]]
 
 def same_classification(e: pd.DataFrame):
     e=e.to_numpy()
@@ -129,17 +123,9 @@ def main():
     train = pd.read_csv("train.csv")
     test = pd.read_csv("test.csv")
     tree = Digraph(name="Decision Tree", filename="dtl")
-    tree.node("attr_1", label="a1")
-    tree.node("attr_2", label="a2")
-    tree.node("attr_3", label="a3")
-    tree.edge(tail_name="attr_1", head_name="attr_2")
-    tree.edge(tail_name="attr_1", head_name="attr_3")
-    #tree.render(view=True)
-
-
-    res = learn_decision_tree(train,train.columns,tree)
-    res.print_children()
-
+    print(train)
+    res = learn_decision_tree(train, train.columns, tree)
+    tree.render(view=True)
 
 
 if __name__ == "__main__":
