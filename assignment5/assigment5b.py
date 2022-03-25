df42ce77df8df880e32920cd8d2ef752f83b5d13
Anderzz <akjelsrud@gmail.com>
2022-03-25 22:43:55 +0100

    bra

--- assignment5/assigment5b.py
+++ assignment5/assigment5b.py
@@ -1,11 +1,15 @@
+from tabnanny import verbose
 import numpy as np
 import tensorflow as tf
 import pickle
-import statistics
 
 #load data
 with open(file="keras-data.pickle", mode="rb") as file:
     data = pickle.load(file)
+
+#check if running on gpu
+if tf.config.list_physical_devices('GPU'):
+    print("Running on GPU")
 
 x_train = data['x_train']
 y_train = np.array(data['y_train'])
@@ -15,28 +19,19 @@ y_test = np.array(data['y_test'])
 vocab_size = data["vocab_size"]
 max_len = data["max_length"]
 
-lenghts = []
-
-for i in x_train:
-    lenghts.append(len(i))
-
-avg = sum(lenghts)/len(lenghts)
-median = statistics.median(lenghts)
-
-LEN = round(avg)
-
-print("max : ", max_len)
-print("Average : ", round(avg))
+lengths = [len(i) for i in x_train]
+avg = round(sum(lengths) / len(lengths))
 
 #preprocessing
-x_train = tf.keras.preprocessing.sequence.pad_sequences(x_train, maxlen=LEN, padding="post")
-x_test = tf.keras.preprocessing.sequence.pad_sequences(x_test, maxlen=LEN, padding="post")
-print(x_train.shape, y_test.shape)
+x_train = tf.keras.preprocessing.sequence.pad_sequences(x_train, maxlen=avg)
+x_test = tf.keras.preprocessing.sequence.pad_sequences(x_test, maxlen=avg)
+in_len = x_train.shape[1]
 #define the model
 model = tf.keras.Sequential()
-model.add(tf.keras.layers.Embedding(vocab_size, 32, input_length=LEN))
-model.add(tf.keras.layers.LSTM(16, activation='tanh', dropout=0.2,recurrent_dropout=0.0))
+model.add(tf.keras.layers.Embedding(vocab_size, 64, input_length=in_len))
+model.add(tf.keras.layers.LSTM(32, activation='tanh', recurrent_activation='sigmoid', dropout=0.2))
+model.add(tf.keras.layers.Dense(16,activation="tanh",kernel_regularizer=tf.keras.regularizers.l1_l2(l1=1e-5, l2=1e-4)))
 model.add(tf.keras.layers.Dense(1,activation="sigmoid"))
 model.compile(optimizer="Adam", loss="binary_crossentropy", metrics=['accuracy'])
-model.fit(x_train, y_train, epochs=1, batch_size=32, verbose=1)
-model.evaluate(x_test, y_test, verbose=0)
+model.fit(x_train, y_train, epochs=5, batch_size=32, verbose=True)
+model.evaluate(x_test, y_test, verbose=True)
